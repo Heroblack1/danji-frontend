04024943b569607ed671cedea4eeb4158b1325d5
Heroblack1 <danjumahero@gmail.com>
2026-08-16 12:59:38 +0100

    scroll to top55

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
 import "./App.css";
 import Hompage from "./hompage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Pricing from "./pricing";
 import About from "./about";
 import Contact from "./contact";
