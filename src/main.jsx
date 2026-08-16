8285d411e3e201cce967640df7c11625b43172c5
Heroblack1 <danjumahero@gmail.com>
2026-08-16 13:06:56 +0100

    scroll to top666

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import "./index.css";
 import App from "./App.jsx";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 // import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
